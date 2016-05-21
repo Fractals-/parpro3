48968d9247107c47d3d00ae0fab72815fb0b0460
Fractals- <fractals.me@gmail.com>
2016-05-21 14:55:22 +0200

    debug attempt 1

--- parsort.cc
+++ parsort.cc
@@ -1,82 +1,93 @@
 #include <stdlib.h>
 #include <cstdio>
 #include <cmath>
-#include <ctime>
 //#include "mpi.h"
 
 #include "heapsort.h"
 #include "blocksort.h"
 
 
-#define N 51ULL//160000000000ULL
+#define N 100ULL//160000000000ULL
 #define BASE_SEED1 0x1234abcd
 #define BASE_SEED2 0x10203040
 #define BASE_SEED3 0x40e8c724
 #define BASE_SEED4 0x79cbba1d
 #define BASE_SEED5 0xac7bd459
 
-/* Code taken from the GLIBC manual.
- *
- * Subtract the ‘struct timespec’ values X and Y,
- * storing the result in RESULT.
- * Return 1 if the difference is negative, otherwise 0.
- */
-static int
-timespec_subtract (struct timespec *result,
-                   struct timespec *x,
-                   struct timespec *y)
-{
-  /* Perform the carry for the later subtraction by updating y. */
-  if (x->tv_nsec < y->tv_nsec) {
-    int nsec = (y->tv_nsec - x->tv_nsec) / 1000000000 + 1;
-    y->tv_nsec -= 1000000000 * nsec;
-    y->tv_sec += nsec;
-  }
-  if (x->tv_nsec - y->tv_nsec > 1000000000) {
-    int nsec = (x->tv_nsec - y->tv_nsec) / 1000000000;
-    y->tv_nsec += 1000000000 * nsec;
-    y->tv_sec -= nsec;
-  }
-
-  /* Compute the time remaining to wait.
-     tv_nsec is certainly positive. */
-  result->tv_sec = x->tv_sec - y->tv_sec;
-  result->tv_nsec = x->tv_nsec - y->tv_nsec;
-
-  /* Return 1 if result is negative. */
-  return x->tv_sec < y->tv_sec;
-}
-
-
 static int rank;
 static int mpi_size;
 size_t n;
 
 
-
 // *************************************************************************************
 
 void mergeSortComponents( int *my_array ){
+  int step = 1; // Stores the current step size
+  int nstep, mod_rank, comm_size = 50;
+  MPI_Status status;
 
+  size_t i, new_n, local, filled, other;
+  int *comm_array = (int*) malloc(sizeof(int) * comm_size);
+
+  // Perform stepwise reduction
+  while ( step != mpi_size ) {
+    nstep = 2 * step;
+    mod_rank = rank % nstep;
+
+    if ( mod_rank == 0 ) {
+      //MPI_Recv(&n_add, 1, MPI_UNSIGNED_LONG_LONG, rank + step, 0, MPI_COMM_WORLD, &status);
+      new_n = 2 * n;
+      local = n - 1;
+      filled = new_n - 1;
+      my_array = (int*) realloc(my_array, sizeof(int) * new_n);
+
+      for ( i = 0; i < n; i += comm_size ) {
+        MPI_Recv(&comm_array[0], comm_size, MPI_INT, rank + step, 0, MPI_COMM_WORLD);
+        other = comm_size - 1;
+        while ( other != ULLONG_MAX && local != ULLONG_MAX ) {
+          if ( my_array[local] < comm_array[other] ){
+            my_array[filled] = comm_array[other];
+            other--;
+          }
+          else {
+            my_array[filled] = my_array[local];
+            local--;
+          }
+          filled--;
+        }
+        while ( other != ULLONG_MAX ) {
+          my_array[filled] = comm_array[other];
+          other--;
+          filled--;
+        }
+      }
+      n = new_n;
+    }
+    else if ( mod_rank - step == 0 ) {
+      //MPI_Send(&n, 1, MPI_UNSIGNED_LONG_LONG, rank - step, 0, MPI_COMM_WORLD);
+      for ( i = 0; i < n; i += comm_size )
+        MPI_Send(&my_array[i], comm_size, MPI_INT, rank - step, 0, MPI_COMM_WORLD);
+    }
+
+    step = nstep;
+  }
   // TODO: implement mergeSort for merging processor sorted arrays
+
+  // At each step:
+  //    - allocate the required additional space
+  //    - Until done: send/recv 'x' (sorted) integers, merge them into empty part 
+  //                  with the processors own sorted array (only as far as send integers go)
 }
 
 // *************************************************************************************
 
 
 int main( int argc, char **argv ){
-  ///////////////////////////////////////////////////////////////
-  /*
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-  */
-  rank = 0;
-  mpi_size = 1;
-  ///////////////////////////////////////////////////////////////
 
   n = N / mpi_size;
-
   int *my_array = (int*) malloc(sizeof(int) * n);
 
   /* Initialize the random number generator for the given BASE_SEED
@@ -112,16 +123,12 @@ int main( int argc, char **argv ){
   for (size_t i = 0; i < n; i++)
     my_array[i] = rand();
 
-  ////////////////////////////////////////////////////////////////////////////////
-  // double start_time = MPI_Wtime();
-  struct timespec start_time;
-  clock_gettime(CLOCK_REALTIME, &start_time);
-  ////////////////////////////////////////////////////////////////////////////////
+  double start_time = MPI_Wtime();
 
   // Debug output
   fprintf(stdout, "--------------------\n");
   for (size_t i = 0; i < n; i++)
-    fprintf(stderr, "%d\n", my_array[i]);
+    fprintf(stderr, "%d: %d\n", rank, my_array[i]);
   fprintf(stdout, "--------------------\n");
 
   // TODO: sort array (of this processor) (heapsort or mergesort?)
@@ -131,34 +138,24 @@ int main( int argc, char **argv ){
   // Debug output
   fprintf(stdout, "--------------------\n");
   for (size_t i = 0; i < n; i++)
-    fprintf(stderr, "%d\n", my_array[i]);
+    fprintf(stderr, "%d: %d\n", rank, my_array[i]);
   fprintf(stdout, "--------------------\n");
 
   // TODO: combine sorted arrays
   mergeSortComponents(my_array);
 
-  // double elapsed_time = MPI_Wtime - start_time;
+  double elapsed_time = MPI_Wtime - start_time;
 
   // Output part of sorted array
   if ( rank == 0 ) {
     for (size_t i = 0; i < N; i += 10000)
       fprintf(stdout, "%d\n", my_array[i]);
     fprintf(stdout, "--------------------\n");
-    // fprintf(stdout, "Execution time: %.2f\n", elapsed_time);
+    fprintf(stdout, "Execution time: %.2f\n", elapsed_time);
   }
 
-  ////////////////////////////////////////////////////////////////////////////////
-  struct timespec end_time, elapsed_time;
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  timespec_subtract(&elapsed_time, &end_time, &start_time);
-  double elapsed = (double)elapsed_time.tv_sec +
-      (double)elapsed_time.tv_nsec / 1000000000.0;
-  fprintf(stderr, "elapsed time: %.4f s\n", elapsed);
-  ///////////////////////////////////////////////////////////////////////////////
+  fprintf(stdout, "%d: Execution time: %.2f\n", rank, elapsed_time);
 
   free(my_array);
-  /*
-  fprintf(stdout, "%d: Execution time: %.2f\n", rank, elapsed_time);
   MPI_Finalize();
-  */
 }
