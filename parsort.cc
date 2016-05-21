8dbf06585c451dd314130125ab18d1ca1d608297
Fractals- <fractals.me@gmail.com>
2016-05-21 17:57:37 +0200

    woef

--- parsort.cc
+++ parsort.cc
@@ -70,8 +70,8 @@ void mergeSortComponents( int *&my_array ){
       n = new_n;
     }
     else if ( mod_rank - step == 0 ) {
-      for ( i = n - comm_size; i > 1; i -= comm_size )
-        MPI_Send(&my_array[i - 1], comm_size, MPI_INT, rank - step, 0, MPI_COMM_WORLD);
+      for ( i = n - 1; i > 0; i -= comm_size )
+        MPI_Send(&my_array[i], comm_size, MPI_INT, rank - step, 0, MPI_COMM_WORLD);
       MPI_Send(&my_array[0], comm_size, MPI_INT, rank - step, 0, MPI_COMM_WORLD);
     }
 
