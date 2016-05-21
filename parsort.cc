ce3fa79a87d7e7701da66ecb5a1ae6a778c52ba3
Fractals- <fractals.me@gmail.com>
2016-05-21 17:23:39 +0200

    d

--- parsort.cc
+++ parsort.cc
@@ -53,10 +53,12 @@ void mergeSortComponents( int *my_array ){
         while ( other >= 0 && local != ULONG_MAX ) {
           fprintf(stdout, "%lu, %lu, %d: %d, %d\n", filled, local, other, my_array[local], comm_array[other]);
           if ( my_array[local] < comm_array[other] ){
+            fprintf(stdout, "tada\n");
             my_array[filled] = comm_array[other];
             other--;
           }
           else {
+            fprintf(stdout, "tada2\n");
             my_array[filled] = my_array[local];
             local--;
           }
